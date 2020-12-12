0f17da44a98e57324425a0c0d2a030064d9c36d7
Michael Cummings <dragonrun1@gmail.com>
2020-12-12 02:35:59 -0800

    Minor re-formatting of up.sql file.

--- migrations/20201208000001_initial/up.sql
+++ migrations/20201208000001_initial/up.sql
@@ -72,14 +72,15 @@ CREATE TABLE people
 INSERT INTO genders
     (id, gender_identity, sex)
 VALUES
-       ('Aj_ZFxDQZMTZvy4VS4xPXW', 'female', 'male'),
-       ('BG2edTSZ9CDZIzQY5Q3Bmi', 'female', 'female'),
-       ('BuuaJbG8lBFJ1WMiyAM2GS', 'male', 'male'),
-       ('CWZiDGemBFha7Oy-XEyTUx', 'male', 'female'),
-       ('XNV_rXUFQvqQfvqJLSHP5w', NULL, 'female'),
-       ('sFJPpw75SSmFiF1wnfbZVg', NULL, 'male');
+    ('Aj_ZFxDQZMTZvy4VS4xPXW', 'female', 'male'),
+    ('BG2edTSZ9CDZIzQY5Q3Bmi', 'female', 'female'),
+    ('BuuaJbG8lBFJ1WMiyAM2GS', 'male', 'male'),
+    ('CWZiDGemBFha7Oy-XEyTUx', 'male', 'female'),
+    ('XNV_rXUFQvqQfvqJLSHP5w', NULL, 'female'),
+    ('sFJPpw75SSmFiF1wnfbZVg', NULL, 'male');
 -- Pronouns
 INSERT INTO pronouns (id, object, possessive, subject)
-VALUES ('B01Ij6SSS8OT5S0hoJdPcw', 'him', 'his', 'he'),
-       ('pjKF25EwT_G2bnof7b5yJQ', 'them', 'theirs', 'they'),
-       ('twPhMNLuTkG9HcZm59kyow', 'her', 'hers', 'she');
+VALUES
+    ('B01Ij6SSS8OT5S0hoJdPcw', 'him', 'his', 'he'),
+    ('pjKF25EwT_G2bnof7b5yJQ', 'them', 'theirs', 'they'),
+    ('twPhMNLuTkG9HcZm59kyow', 'her', 'hers', 'she');
