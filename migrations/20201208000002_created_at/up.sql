ee1c46952569afbc179c3e2b7869c91535a60eda
Michael Cummings <dragonrun1@gmail.com>
2021-01-13 08:58:12 -0800

    Changed created_at migration to use CURRENT_TIMESTAMP default.

--- migrations/20201208000002_created_at/up.sql
+++ migrations/20201208000002_created_at/up.sql
@@ -2,15 +2,8 @@
 -- Must add the column with a default set because of NOT NULL so any existing
 -- rows are updated.
 ALTER TABLE genders
-    ADD COLUMN created_at DATETIME NOT NULL DEFAULT '2020-12-08 00:00:02';
+    ADD COLUMN created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE people
-    ADD COLUMN created_at DATETIME NOT NULL DEFAULT '2020-12-08 00:00:02';
+    ADD COLUMN created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE pronouns
-    ADD COLUMN created_at DATETIME NOT NULL DEFAULT '2020-12-08 00:00:02';
--- Can now drop the defaults so column must be given a value during future inserts.
-ALTER TABLE genders
-    ALTER COLUMN created_at DROP DEFAULT;
-ALTER TABLE people
-    ALTER COLUMN created_at DROP DEFAULT;
-ALTER TABLE pronouns
-    ALTER COLUMN created_at DROP DEFAULT;
+    ADD COLUMN created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
